e876ab65313be09e47e020038d2aeac71fc98ba4
Prasannadevi R <138461679+prasannadeviramesh@users.noreply.github.com>
2025-05-30 10:51:18 +0530

    Create 1683.Invalid Tweets.sql

---
+++ leetcode-sql/1683.Invalid Tweets.sql
@@ -0,0 +1,35 @@
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+
+CODE:
+# Write your MySQL query statement below
+select tweet_id
+from tweets
+where char_length(content)>15;
+
+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet.
